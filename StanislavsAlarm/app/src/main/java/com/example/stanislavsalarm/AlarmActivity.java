c22136717bd340492edf12fb699919a722073d2b
Stepashka69R <Stasek53l13@gmail.com>
2023-05-01 12:07:10 +0300

    AlarmOnAndroid

---
+++ StanislavsAlarm/app/src/main/java/com/example/stanislavsalarm/AlarmActivity.java
@@ -0,0 +1,42 @@
+package com.example.stanislavsalarm;
+
+import android.content.Context;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    Ringtone ringtone;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_alarm);
+
+        Uri notificationUri = RingtoneManager.getDefaultUri((RingtoneManager.TYPE_ALARM));
+                ringtone = RingtoneManager.getRingtone(this, notificationUri);
+                if (ringtone == null) {
+                    notificationUri = RingtoneManager.getDefaultUri((RingtoneManager.TYPE_RINGTONE));
+                    ringtone = RingtoneManager.getRingtone(this, notificationUri);
+                }
+                if (ringtone != null) {
+                    ringtone.play();
+                }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (ringtone != null && ringtone.isPlaying()) {
+            ringtone.stop();
+        }
+        super.onDestroy();
+    }
+}
